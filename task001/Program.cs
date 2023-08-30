76195443dd17a2b7c7ce76627f396ffcaf016f23
Alexander Kamaev <qalex2@yandex.ru>
2023-08-30 12:58:01 +0300

    Написана функция создания динамического массива строк посредством ввода с клавиатуры

--- task001/Program.cs
+++ task001/Program.cs
@@ -6,3 +6,47 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+void PositiveNumber(List<int> array)
+{  
+    int count = 0;
+    for (int i = 0; i < array.Count; i++)
+    {
+        if (array[i] > 0) count++;          
+    }
+    Console.WriteLine($"Количество чисел > 0 в массиве - {count} !!");
+}
+
+void PrintArray(double[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(massive[i,j],2)}\t", -5);
+        }
+        Console.WriteLine();
+    }
+}
+
+List<string> GetDynamicArray()
+{
+    List<string> dArray = new List<string>();
+    string str;
+    bool isValidStr = false;
+    Console.WriteLine("Введите строковые переменные любой длины (окончание ввода пустая строка):");    
+    do
+    {
+        Console.Write("Следующая переменная: ");
+        string input = Console.ReadLine();
+        if (str.Length > 0) isValidStrNumber = true;
+        {              
+            dArray.Add(str);             
+        }
+    }    
+    while(isValidStr);
+    Console.WriteLine($"Сгенерирован массив - [{String.Join(", ", dArray)}]");
+    return dArray;
+}
+
+PositiveNumber(GetDynamicArray());
